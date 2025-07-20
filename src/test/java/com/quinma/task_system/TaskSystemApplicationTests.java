785faa44096af6d79e32a55cfa30f60b9d3f44ca
Neid-09 <neiderqm09@gmail.com>
2025-07-19 21:56:28 -0500

    Agregar archivos de configuración y estructura inicial del proyecto Spring Boot - Sistema de tareas

---
+++ src/test/java/com/quinma/task_system/TaskSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quinma.task_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
